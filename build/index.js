1a0f16e4c6879b25ddda75e95d689a225541ebd5
Martin <martin@wiseweb.solutions>
2018-08-12 03:27:30 -0400

    Cleaning it up

--- build/index.js
+++ build/index.js
@@ -1,74 +1,71 @@
-import babel from "gulp-babel";
-import concat from "gulp-concat";
-import del from "del";
-import gulp from "gulp";
-import sass from "gulp-sass";
-import pug from "gulp-pug";
-import uglify from "gulp-uglify";
-import path from "path";
-import browserSync from "browser-sync";
+import babel from 'gulp-babel'
+import concat from 'gulp-concat'
+import del from 'del'
+import gulp from 'gulp'
+import sass from 'gulp-sass'
+import plumber from 'gulp-plumber'
+import pug from 'gulp-pug'
+import uglify from 'gulp-uglify'
+import path from 'path'
+import browserSync from 'browser-sync'
 
-const server = browserSync.create();
+const server = browserSync.create()
 
 const paths = {
-  src: "./src",
-  dest: "./dist"
-};
+	src: path.resolve(__dirname, '../src'),
+	dest: path.resolve(__dirname, '../dist'),
+}
 
-const clean = () => del(["dist"]);
+const clean = () => del(['dist'])
 
 const scripts = () =>
-  gulp
-    .src(`${paths.src}/resources/scripts/*.js`, { sourcemaps: true })
-    .pipe(babel())
-    .pipe(uglify())
-    .pipe(concat("index.min.js"))
-    .pipe(gulp.dest(`${paths.dest}/assets/js`));
+	gulp
+		.src(`${paths.src}/resources/scripts/*.js`, { sourcemaps: true })
+		.pipe(babel())
+		.pipe(uglify())
+		.pipe(concat('index.min.js'))
+		.pipe(gulp.dest(`${paths.dest}/assets/js`))
 
 const compileSass = () =>
-  gulp
-    .src(`${paths.src}/resources/sass/*.sass`)
-    .pipe(sass())
-    .pipe(gulp.dest(`${paths.dest}/assets/css`));
+	gulp
+		.src(`${paths.src}/resources/sass/main.sass`)
+		.pipe(plumber())
+		.on('error', function(e) {
+			console.error(e)
+			this.emit('end')
+		})
+		.pipe(sass())
+		.pipe(gulp.dest(`${paths.dest}/assets/css`))
 
 const compilePug = () =>
-  gulp
-    .src(`${paths.src}/*.pug`)
-    .pipe(pug())
-    .pipe(gulp.dest(`${paths.dest}`));
+	gulp
+		.src(`${paths.src}/*.pug`)
+		.pipe(pug())
+		.pipe(gulp.dest(`${paths.dest}`))
 
 const reload = done => {
-  server.reload();
-  done();
-};
+	server.reload()
+	done()
+}
 
 const serve = done => {
-  server.init({
-    server: {
-      baseDir: paths.dest
-    }
-  });
-  done();
-};
+	server.init({
+		server: {
+			baseDir: paths.dest,
+		},
+	})
+	done()
+}
 
-const buildTasks = [clean, scripts, compilePug, compileSass, serve];
+const buildTasks = [clean, scripts, compilePug, compileSass, serve]
 
-gulp.task("watch", () => {
-  gulp.watch(
-    `${paths.src}/resources/sass/*.sass`,
-    gulp.series(compileSass, reload)
-  );
-  gulp.watch(
-    `${paths.src}/resources/pug/*.pug`,
-    gulp.series(compilePug, reload)
-  );
-  gulp.watch(
-    `${paths.src}/resources/scripts/*.js`,
-    gulp.series(scripts, reload)
-  );
-  return gulp.series(...buildTasks);
-});
+gulp.task('watch', () => {
+	gulp.watch(`./src/resources/sass/*.sass`, gulp.series(compileSass, reload))
+	gulp.watch(`./src/**/*.pug`, gulp.series(compilePug, reload))
+	gulp.watch(`./src/resources/scripts/*.js`, gulp.series(scripts, reload))
+	return gulp.series(...buildTasks)
+})
 
-const dev = gulp.series(...buildTasks, "watch");
+const dev = gulp.series(...buildTasks, 'watch')
 
-export default dev;
+export default dev
