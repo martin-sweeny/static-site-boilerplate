efc88d63d1049226b80689f3d86c4acb1edf0b4c
Martin Sweeny <martin@wiseweb.solutions>
2019-07-04 22:48:27 -0400

    feat: add issue key check

--- commitlint.config.js
+++ commitlint.config.js
@@ -1,1 +1,18 @@
-module.exports = {extends: ['@commitlint/config-conventional']}
+require('dotenv').config()
+
+const rules = {
+	'references-empty': [2, 'never'],
+}
+
+const parserPreset = {
+	parserOpts: {
+		issuePrefixes: [process.env.PROJECT_ISSUE_KEY_PREFIX]
+	}
+}
+
+
+module.exports = {
+	extends: ['@commitlint/config-conventional'],
+	// ...rules
+	// ...parserPreset,
+}
